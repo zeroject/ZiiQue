2366fcac5e9c8524b68869a084f5010b0c0b4d00
Zeroject <47432671+zeroject@users.noreply.github.com>
2023-05-01 10:18:27 +0200

    resposive first step

--- frontend/ziique/lib/main.dart
+++ frontend/ziique/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ziique/BeatBoard/BeatBoard-Widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,21 +11,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      title: 'ZiiQue',
+      home: MyHomePage(title: 'ZiiQue'),
     );
   }
 }
@@ -47,11 +36,9 @@ class _MyHomePageState extends State<MyHomePage> {
       body: LayoutBuilder(
         builder: (BuildContext context, BoxConstraints constraints) {
           if (constraints.maxWidth < 750) {
-            return BuildPhone(context);
-          } else  if (constraints.maxWidth < 1500){
-            return BuildTablet(context);
+            return BeatBoardApp(context);
           } else{
-            return BuildDesktop(context);
+            return BeatBoardDesktop(context);
           }
         },
       ),
