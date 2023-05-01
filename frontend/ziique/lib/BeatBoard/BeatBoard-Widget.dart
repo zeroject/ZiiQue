2eb54c7235de1bc113cac50fc4f459dae78020e6
Nyyberg <92079143+Nyyberg@users.noreply.github.com>
2023-05-01 10:09:46 +0200

    added stateless to bearboard and settings widgets

---
+++ frontend/ziique/lib/BeatBoard/BeatBoard-Widget.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class BeatBoardApp extends StatelessWidget {
+  BeatBoardApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
+
+class BeatBoardDesktop extends StatelessWidget {
+  BeatBoardDesktop({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
+
---
+++ frontend/ziique/lib/Settings/Settings-Widget.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class SettingsPageDesktop extends StatelessWidget {
+  SettingsPageDesktop({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
+
+class SettingsPageMobile extends StatelessWidget {
+  SettingsPageMobile({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
+
+
+
