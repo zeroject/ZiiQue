de4b89b9114db5aecaeb0e3a5489b7594a18dc3f
Nyyberg <92079143+Nyyberg@users.noreply.github.com>
2023-05-02 12:46:50 +0200

    added code to login and create widget

---
+++ ziique/lib/login-create/Create-Widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class CreateDesktop extends StatelessWidget {
+  CreateDesktop({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
+
+class CreateMobile extends StatelessWidget {
+  CreateMobile({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
+
+class Create extends StatelessWidget {
+  Create({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
+
+
---
+++ ziique/lib/login-create/Login-Widget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class LoginDesktop extends StatelessWidget {
+  LoginDesktop({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
+
+class LoginMobile extends StatelessWidget {
+  LoginMobile({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
