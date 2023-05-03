fb016173349f9d88102dcdf521b57b88727023e6
Nyyberg <92079143+Nyyberg@users.noreply.github.com>
2023-05-03 11:04:26 +0200

    login-page UI finished

--- ziique/lib/login-create/Login-Widget.dart
+++ ziique/lib/login-create/Login-Widget.dart
@@ -7,7 +7,68 @@ class LoginDesktop extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/blue-background.png"),
+              fit: BoxFit.none,
+            )
+        ),
+        child: Container(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 40,
+              ),
+              SizedBox(
+                height: 250,
+                child: Image.asset("assets/images/Ziique-logo.png"),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(
+                    color: Colors.white
+                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(10))
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      SizedBox(height: 20,),
+                      SizedBox(
+                        width: 400,
+                          child: TextFormField(decoration: InputDecoration(hintText: "Email"),)),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      SizedBox(
+                        width: 400,
+                          child: TextFormField(obscureText: true, decoration: InputDecoration(hintText: "Password"),)),
+                      SizedBox(
+                        height: 50,
+                      ),
+                      SizedBox(
+                        width: 200,
+                          height: 40,
+                          child: ElevatedButton( style: ElevatedButton.styleFrom(backgroundColor: Color.fromARGB(255, 217, 217, 217)),onPressed: (){}, child: Text("Log In", style: TextStyle(fontSize: 24, color: Colors.black),))
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container()
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
@@ -20,7 +81,22 @@ class LoginMobile extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/blue-background.png"),
+            fit: BoxFit.none,
+          )
+        ),
+        child: Container(
+          child: Column(
+            children: [
+              Container(),
+              Container()
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
