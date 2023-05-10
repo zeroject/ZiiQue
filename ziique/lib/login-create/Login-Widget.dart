3e2d47890be44c1a57b8715d1451c4d7ea8b6782
Zeroject <47432671+zeroject@users.noreply.github.com>
2023-05-10 09:10:46 +0200

    Update Login-Widget.dart

--- ziique/lib/login-create/Login-Widget.dart
+++ ziique/lib/login-create/Login-Widget.dart
@@ -84,23 +84,78 @@ class LoginDesktop extends StatelessWidget {
 
 class LoginMobile extends StatelessWidget {
   LoginMobile(BuildContext context);
-
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
+      appBar: AppBar(title: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('assets/images/ZiiQue-Logo.png',
+            scale: 12, alignment: Alignment.topCenter,
+          ),
+          const SizedBox(
+              width: 100
+          ),
+        ],
+      ), backgroundColor: Color.fromARGB(255, 44, 41, 41),),
       body: Container(
         decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/blue-background.png"),
-            fit: BoxFit.none,
-          )
+            image: DecorationImage(
+              image: AssetImage("assets/images/blue-background.png"),
+              fit: BoxFit.none,
+            )
         ),
         child: Container(
           child: Column(
             children: [
-              Container(),
+              SizedBox(
+                height: 40,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(
+                        color: Colors.white
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(10))
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      SizedBox(height: 20,),
+                      SizedBox(
+                          width: 400,
+                          child: TextFormField(controller: emailController,decoration: InputDecoration(hintText: "Email"),)),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      SizedBox(
+                          width: 400,
+                          child: TextFormField(controller: passwordController ,obscureText: true, decoration: InputDecoration(hintText: "Password"),)),
+                      SizedBox(
+                        height: 50,
+                      ),
+                      SizedBox(
+                          width: 200,
+                          height: 40,
+                          child: ElevatedButton( style: ElevatedButton.styleFrom(backgroundColor: Color.fromARGB(255, 217, 217, 217)),
+                              onPressed: (){
+                                Functions().ValidateAndSumbit(emailController.text, passwordController.text, context);
+                              },
+                              child: Text("Log In", style: TextStyle(fontSize: 24, color: Colors.black),))
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
               Container()
             ],
           ),
