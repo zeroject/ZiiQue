aebdefe3e38e561d7e36b4aead22074ce195ca55
Nyyberg <92079143+Nyyberg@users.noreply.github.com>
2023-05-11 10:28:10 +0200

    added notifications tab in settings page

--- ziique/lib/Settings/Settings-Widget.dart
+++ ziique/lib/Settings/Settings-Widget.dart
@@ -25,6 +25,10 @@ class SettingsPageMobile extends StatefulWidget {
 }
 
 class _SettingsPageMobileState extends State<SettingsPageMobile> {
+    bool light = true;
+    bool light2 = true;
+    bool light3 = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,7 +38,9 @@ class _SettingsPageMobileState extends State<SettingsPageMobile> {
             return Build1(context);
           } else if(scene.contains('Payment')){
             return Build2(context);
-          };
+          } else if(scene.contains('Notifications')){
+            return Build3(context);
+          }
           return Build1(context);
         }
     ),
@@ -84,7 +90,7 @@ Widget Build1(BuildContext context){
                       children: [
                         OutlinedButton(onPressed: (){scene = 'Account'; setState(() {});}, child: Text('Account', style: TextStyle(fontSize: 24, color: Colors.white),)),
                         OutlinedButton(onPressed: (){scene = 'Payment'; setState(() {});}, child: Text('Payment', style: TextStyle(fontSize: 24, color: Colors.white),)),
-                        OutlinedButton(onPressed: (){}, child: Text('Notifications', style: TextStyle(fontSize: 24, color: Colors.white),)),
+                        OutlinedButton(onPressed: (){scene = 'Notifications'; setState(() {});}, child: Text('Notifications', style: TextStyle(fontSize: 24, color: Colors.white),)),
                         OutlinedButton(onPressed: (){}, child: Text('Security', style: TextStyle(fontSize: 24, color: Colors.white),)),
                         OutlinedButton(onPressed: (){}, child: Text('Friends', style: TextStyle(fontSize: 24, color: Colors.white),)),
                       ],
@@ -163,7 +169,7 @@ Widget Build2(BuildContext context){
                     children: [
                       OutlinedButton(onPressed: (){scene = 'Account'; setState(() {});}, child: Text('Account', style: TextStyle(fontSize: 24, color: Colors.white),)),
                       OutlinedButton(onPressed: (){scene = 'Payment'; setState(() {});}, child: Text('Payment', style: TextStyle(fontSize: 24, color: Colors.white),)),
-                      OutlinedButton(onPressed: (){}, child: Text('Notifications', style: TextStyle(fontSize: 24, color: Colors.white),)),
+                      OutlinedButton(onPressed: (){scene = 'Notifications'; setState(() {});}, child: Text('Notifications', style: TextStyle(fontSize: 24, color: Colors.white),)),
                       OutlinedButton(onPressed: (){}, child: Text('Security', style: TextStyle(fontSize: 24, color: Colors.white),)),
                       OutlinedButton(onPressed: (){}, child: Text('Friends', style: TextStyle(fontSize: 24, color: Colors.white),)),
                     ],
@@ -188,7 +194,7 @@ Widget Build2(BuildContext context){
                                 children: [
                                   Text('Visa Credit Card', style: TextStyle(fontSize: 22, color: Colors.white),),
                                   Text('Ends in 5847', style: TextStyle(fontSize: 22, color: Colors.white),),
-                                  Text('06/25', style: TextStyle(fontSize: 22, color: Colors.white),)
+                                  Text('06/25', style: TextStyle(fontSize: 22, color: Colors.white),),
                                 ],
                               ),
                             ),
@@ -209,4 +215,83 @@ Widget Build2(BuildContext context){
     ),
   );
 }
+  Widget Build3(BuildContext context){
+
+    return Scaffold(
+      appBar: AppBar(title: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('assets/images/ZiiQue-Logo.png',
+            scale: 12, alignment: Alignment.topCenter,
+          ),
+          const SizedBox(
+              width: 10
+          ),
+        ],
+      ), backgroundColor: Color.fromARGB(255, 44, 41, 41),),
+      body: Container(
+        constraints: BoxConstraints.expand(),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/grey-background.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 600,
+              height: MediaQuery.of(context).size.height - 56,
+              color: Colors.black26.withOpacity(1),
+
+              child:
+              Container(
+                child: Row(
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        OutlinedButton(onPressed: (){scene = 'Account'; setState(() {});}, child: Text('Account', style: TextStyle(fontSize: 24, color: Colors.white),)),
+                        OutlinedButton(onPressed: (){scene = 'Payment'; setState(() {});}, child: Text('Payment', style: TextStyle(fontSize: 24, color: Colors.white),)),
+                        OutlinedButton(onPressed: (){scene = 'Notifications'; setState(() {});}, child: Text('Notifications', style: TextStyle(fontSize: 24, color: Colors.white),)),
+                        OutlinedButton(onPressed: (){}, child: Text('Security', style: TextStyle(fontSize: 24, color: Colors.white),)),
+                        OutlinedButton(onPressed: (){}, child: Text('Friends', style: TextStyle(fontSize: 24, color: Colors.white),)),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(30.0),
+                      child: Container(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(children: [
+                                Text('Friend Request Notifications', style: TextStyle(fontSize: 18, color: Colors.white), ),
+                                Switch(value: light, activeColor: Colors.blue, onChanged: (bool value){setState(() {light = value;});}),
+                              ],),
+                              Row(children: [
+                                Text('General Notifications', style: TextStyle(fontSize: 18, color: Colors.white),),
+                                Switch(value: light2, activeColor: Colors.blue, onChanged: (bool value){setState(() {light2 = value;});}),
+                              ],),
+                              Row(children: [
+                                Text('Notifications About Updates', style: TextStyle(fontSize: 18, color: Colors.white),),
+                                Switch(value: light3, activeColor: Colors.blue, onChanged: (bool value){setState(() {light3 = value;});}),
+                              ],),
+                            ],
+                          )
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
 }
