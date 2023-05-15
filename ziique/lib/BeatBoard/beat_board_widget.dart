76609fd4ae84a38e377dc14d0b9677ee56a5007f
Zeroject <47432671+zeroject@users.noreply.github.com>
2023-05-15 14:29:18 +0200

    performance update

--- ziique/lib/BeatBoard/beat_board_widget.dart
+++ ziique/lib/BeatBoard/beat_board_widget.dart
@@ -20,11 +20,16 @@ class BeatBoardDesktop extends StatefulWidget {
 }
 
 class _BeatBoardDesktopState extends State<BeatBoardDesktop> {
+  final Future<String> _calculation = Future<String>.delayed(
+    const Duration(milliseconds: 50),
+    () => 'Data Loaded',
+  );
 
   Color beatInfo = const Color.fromARGB(255, 72, 72, 72);
   Color beatNorm = const Color.fromARGB(255, 0, 178, 255);
   Color beatNormPress = const Color.fromARGB(255, 0, 105, 147);
-  List<bool> boolList = List.generate(numberOfRows * ((numberOfBars * 4) + 1), (index) => false);
+  List<bool> boolList =
+      List.generate(numberOfRows * ((numberOfBars * 4) + 1), (index) => false);
   Alpha alpha = Alpha();
 
   @override
@@ -83,40 +88,67 @@ class _BeatBoardDesktopState extends State<BeatBoardDesktop> {
         child: Column(
           children: [
             Center(
-              child: Container(
-
-              ),
+              child: Container(),
             ),
-            Expanded(
-              child: GridView.count(
-                padding: const EdgeInsets.all(8),
-                crossAxisCount: (numberOfBars * 4) + 1,
-                children: [
-                  for (var i = 0; i < ((numberOfBars * 4) + 1) * numberOfRows; i++)
-                    Container(
-                      color: Colors.black,
-                      child: Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: (alpha.calcGreenBut(i, numberOfBars) == 0) ? Colors.green : BeatColor(bar: numberOfBars).getColor(i) ? (boolList[i] == true) ? Colors.grey[900] : Colors.grey : (boolList[i] == true) ? Colors.grey[900] : Colors.blueGrey
+            FutureBuilder(
+              future: _calculation,
+              builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                if (snapshot.hasData) {
+                  return Expanded(
+                    child: GridView.count(
+                      padding: const EdgeInsets.all(8),
+                      crossAxisCount: (numberOfBars * 4) + 1,
+                      children: [
+                        for (var i = 0;
+                            i < ((numberOfBars * 4) + 1) * numberOfRows;
+                            i++)
+                          Container(
+                            color: Colors.black,
+                            child: Padding(
+                              padding: const EdgeInsets.all(4.0),
+                              child: StatefulBuilder(
+                                builder: (context, reload) {
+                                  return ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                        backgroundColor: (alpha.calcGreenBut(
+                                                    i, numberOfBars) ==
+                                                0)
+                                            ? Colors.green
+                                            : BeatColor(bar: numberOfBars)
+                                                    .getColor(i)
+                                                ? (boolList[i] == true)
+                                                    ? Colors.grey[900]
+                                                    : Colors.grey
+                                                : (boolList[i] == true)
+                                                    ? Colors.grey[900]
+                                                    : Colors.blueGrey),
+                                    onPressed: () {
+                                      reload(() {
+                                        boolList[i] = !boolList[i];
+                                        maxRange = (numberOfBars * 4);
+                                        minRange = 1;
+                                      });
+                                    },
+                                    child: Text(
+                                      alpha.getAlphebat(i, numberOfBars),
+                                      style:
+                                          const TextStyle(color: Colors.white),
+                                    ),
+                                  );
+                                },
+                              ),
+                            ),
                           ),
-                          onPressed: () {
-                            setState(() {
-                              boolList[i] = !boolList[i];
-                              maxRange = (numberOfBars * 4);
-                              minRange = 1;
-                              });
-                          },
-                          child: Text(
-                            alpha.getAlphebat(i, numberOfBars),
-                            style: const TextStyle(color: Colors.white),
-                          ),
-                        ),
-                      ),
+                      ],
                     ),
-                ],
-              ),
+                  );
+                } else {
+                  return const SizedBox(
+                    height: 200,
+                    child: Center(child: CircularProgressIndicator()),
+                  );
+                }
+              },
             ),
           ],
         ),
@@ -126,7 +158,7 @@ class _BeatBoardDesktopState extends State<BeatBoardDesktop> {
 }
 
 class Alpha {
-  Alpha(){
+  Alpha() {
     for (int i = 65; i <= 90; i++) {
       alphabets.add(String.fromCharCode(i));
     }
@@ -135,43 +167,38 @@ class Alpha {
   int greenBut = 0;
   List<String> alphabets = [];
 
-
-  int calcGreenBut(i, numberOfBars){
+  int calcGreenBut(i, numberOfBars) {
     greenBut = i % ((numberOfBars * 4) + 1);
     return greenBut;
   }
 
   String getAlphebat(i, numberOfBars) {
-    if (greenBut == 0){
-      return alphabets[i == 1 ? 1 : i~/((numberOfBars * 4) + 1)];
+    if (greenBut == 0) {
+      return alphabets[i == 1 ? 1 : i ~/ ((numberOfBars * 4) + 1)];
     }
-    return i;
+    return i.toString();
   }
-
 }
 
-class BeatColor{
+class BeatColor {
   BeatColor({required this.bar});
+
   final int bar;
 
-  bool getColor(int i){
+  bool getColor(int i) {
     if (kDebugMode) {
-      print("beatIndex: $i maxRange: $maxRange${i == maxRange ? " true" : " false"}");
+      print(
+          "beatIndex: $i maxRange: $maxRange${i == maxRange ? " true" : " false"}");
     }
-    if (i == maxRange)
-    {
-      maxRange += (numberOfBars * 4) +1 ;
-      minRange += (numberOfBars * 4) +1 ;
-      maxRange += (numberOfBars * 4) +1 ;
-      minRange += (numberOfBars * 4) +1;
+    if (i == maxRange) {
+      maxRange += (numberOfBars * 4) + 1;
+      minRange += (numberOfBars * 4) + 1;
+      maxRange += (numberOfBars * 4) + 1;
+      minRange += (numberOfBars * 4) + 1;
       return true;
-    }
-    else if (i >= minRange && i <= maxRange)
-    {
+    } else if (i >= minRange && i <= maxRange) {
       return true;
-    }
-    else
-    {
+    } else {
       return false;
     }
   }
