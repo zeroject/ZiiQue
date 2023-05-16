d9ed15401a38e14d7c0788777a63f9f3af38de7a
Zeroject <47432671+zeroject@users.noreply.github.com>
2023-05-16 13:20:06 +0200

    cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,10 @@
 const functions = require("firebase-functions");
+const node_lame = require("node-lame");
 
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+   functions.logger.info("Hello logs!", {structuredData: true});
+   response.send("Hello from Firebase!");
+ });
