7d21149a964ac60faeca928a74e11df53edfdb71
Nyyberg <92079143+Nyyberg@users.noreply.github.com>
2023-05-17 13:17:57 +0200

    create beatitsession

---
+++ ziique/lib/models/beatitsession.dart
@@ -0,0 +1,15 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:ziique/models/beat.dart';
+
+class BeatItSession{
+  BeatItSession(this.sessionid,this.usersadded,this.creationTime, this.lastModified, this.beat, this.timeschanged, this.timesplayed, this.host, this.versionid  );
+  String sessionid;
+  List<User> usersadded;
+  DateTime creationTime;
+  DateTime lastModified;
+  Beat beat;
+  int timeschanged;
+  int timesplayed;
+  User host;
+  int versionid;
+}
