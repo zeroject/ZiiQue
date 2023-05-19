09734dab753a140be3a408224de851bc125edf72
Casper Fink <cypeyt@gmail.com>
2023-05-19 09:31:31 +0200

    function to add a friend to a session and starting a session

--- ziique/lib/models/beatitsession.dart
+++ ziique/lib/models/beatitsession.dart
@@ -1,15 +1,54 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 import '../../models/user.dart';
 import 'package:ziique/models/beat.dart';
 
+
+class BeatItKeys{
+  static const sessionid = "sessionid";
+  static const usersadded = "usersadded";
+  static const creationTime = "creationTime";
+  static const lastModified = "lastModified";
+  static const beat = "beat";
+  static const timeschanged = "timeschanged";
+  static const timesplayed = "timesplayed";
+  static const host = "host";
+  static const versionid = "versionid";
+}
+
+
 class BeatItSession{
   BeatItSession(this.sessionid,this.usersadded,this.creationTime, this.lastModified, this.beat, this.timeschanged, this.timesplayed, this.host, this.versionid  );
-  String sessionid;
-  List<User> usersadded;
-  DateTime creationTime;
-  DateTime lastModified;
-  Beat beat;
-  int timeschanged;
-  int timesplayed;
-  User host;
-  int versionid;
+  final String sessionid;
+  final List<User> usersadded;
+  final DateTime? creationTime;
+  final DateTime? lastModified;
+  final Beat beat;
+  final int timeschanged;
+  final int timesplayed;
+  final User host;
+  final int versionid;
+
+  BeatItSession.fromMap(this.sessionid, Map<String, dynamic> data)
+      : usersadded = data[BeatItKeys.usersadded],
+        creationTime = (data[BeatItKeys.creationTime] as Timestamp?)?.toDate(),
+        lastModified = (data[BeatItKeys.lastModified] as Timestamp?)?.toDate(),
+        beat = data[BeatItKeys.beat],
+        timeschanged = data[BeatItKeys.timeschanged],
+        timesplayed = data[BeatItKeys.timesplayed],
+        host = User.fromMap(data[BeatItKeys.host]),
+        versionid = data[BeatItKeys.versionid];
+
+  Map<String, dynamic> toMap() {
+    return {
+      BeatItKeys.sessionid: sessionid,
+      BeatItKeys.usersadded: usersadded,
+      BeatItKeys.creationTime: creationTime,
+      BeatItKeys.lastModified: lastModified,
+      BeatItKeys.beat: beat,
+      BeatItKeys.timeschanged: timeschanged,
+      BeatItKeys.timesplayed: timesplayed,
+      BeatItKeys.host: host,
+      BeatItKeys.versionid: versionid
+    };
 }
