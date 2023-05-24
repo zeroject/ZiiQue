c0c5e33bab398ef56c6f1722245b06598bd2ce1d
Ominousity <abs@hotmail.dk>
2023-05-24 12:03:48 +0200

    removed bloc and cubit

--- ziique/lib/BeatBoard/Bloc/beat_board_event.dart
+++
@@ -1,4 +0,0 @@
-part of 'beat_board_bloc.dart';
-
-@immutable
-abstract class BeatBoardEvent {}
--- ziique/lib/BeatBoard/Bloc/beat_board_state.dart
+++
@@ -1,6 +0,0 @@
-part of 'beat_board_bloc.dart';
-
-@immutable
-abstract class BeatBoardState {}
-
-class BeatBoardInitial extends BeatBoardState {}
