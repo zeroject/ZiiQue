046e067249548aaf811ae686eae3363b88d3cf34
Nyyberg <92079143+Nyyberg@users.noreply.github.com>
2023-05-25 17:42:44 +0200

    created a cloud function

--- functions/index.js
+++ functions/index.js
@@ -17,3 +17,30 @@ const logger = require("firebase-functions/logger");
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp({projectId: 'ziique-7aa1b'});
+
+const firestore = admin.firestore();
+
+exports.setBeatString = functions.https.onCall(async (data, context) => {
+    // Check if the session ID is provided
+    if (!data.sessionID) {
+      throw new functions.https.HttpsError('invalid-argument', 'Session ID is missing.');
+    }
+  
+    try {
+      const sessionID = data.sessionID;
+      const beatString = data.beatString;
+  
+      // Create a new document in Firestore with the session ID as the document ID
+      await admin.firestore().collection('Sessions').doc(sessionID).update({
+        beatString: beatString
+      });
+  
+      return { success: true, message: `Session document created with ID: ${sessionID}` };
+    } catch (error) {
+      throw new functions.https.HttpsError('internal', 'An error occurred while creating the session document.', error);
+    }
+  });
